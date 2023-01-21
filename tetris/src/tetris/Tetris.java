07b0175d560b6da2edf4cef7a5eaf5269b3d0645
Pablo Jiménez <Pablo Jimnez@DESKTOP-UEAH07A>
2023-01-21 17:13:02 +0100

    Actualitzacio

--- tetris/src/tetris/Tetris.java
+++ tetris/src/tetris/Tetris.java
@@ -4,6 +4,12 @@ import utils.Utils;
 
 public class Tetris {
 
+    private static boolean[][] tauler;
+    
+    private static peca pecaactual;
+
+     
+
     public static void main(String[] args) {
         //Demanem la mida del tauler en files i columnes, el mínim del tauler serà de 8x4 
         int fila = Utils.LlegirInt("Quantes files vols: ");
@@ -21,36 +27,76 @@ public class Tetris {
             if (columna >= 4) {
                 n_columnes_valid = true;
             } else {
-                columna = Utils.LlegirInt("El tamany mínim del tauler ha de ser de 4x8, torna a introduir les columnes: ");
+                columna = Utils.LlegirInt("El tamany mínim del tauler ha de ser de 8x4, torna a introduir les columnes: ");
             }
         }
-        boolean[][] tauler = new boolean[fila][columna];
-
-    }
-
-    static boolean Mostrarpeca() {
-
-    }
-
-    static boolean MourePecaCostats(boolean tauler[][]) {
-
-    }
-
-    static boolean FerCaureLaPeca(boolean tauler[][]) {
-
-    }
-
-    static void MostrarTaulell(boolean tauler[][]) {
-
-        for (int i = tauler.length - 1; i >= 0; --i) {
-            for (int j = 0; j < tauler[0].length; ++j) {
-                if (tauler[i][j]) {
-                    System.out.print("X");
-                }
-            }
-        }
+        /*Li afegim +4 a la fila perque aquest serà l'espai on es mostaran les peces*/
+        tauler = new boolean[fila + 4][columna];
+        
+        boolean jugant = comprobarfinal();
         
 
+        while (jugant) {
+            
+            pecaactual = new peca();
+
+            Mostrarpeca();
+
+            Mourepeca();
+
+            FerCaureLaPeca();
+
+            MostrarTaulell(tauler, fila, columna);
+        }
+
     }
 
+    public static void Mostrarpeca() {
+
+    }
+
+    public static void MourePeca(boolean tauler[][]) {
+
+        int direccio = Utils.LlegirInt("Introduce la dirección a mover la pieza (1/0): ");
+
+        if (direccio == 1) {
+            pecaactual.moveLeft();
+        } else if (direccio == 0) {
+            pecaactual.moveRight();
+        }
+
+    }
+
+    public static void FerCaureLaPeca(boolean tauler[][]) {
+
+    }
+
+    static void MostrarTaulell(boolean tauler[][], int fila, int columna) {
+
+        for (int i = 0; i < fila; i++) {
+            for (int j = 0; j < columna; j++) {
+                if (tauler[i][j]) {
+                    System.out.print("X ");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+    
+    public static boolean comprobarfinal(){
+        boolean gameover = false;
+        
+        for(int i = 0; i<tauler[0].length; i++){
+            if(tauler[0][i]){
+                gameover = true;
+            }
+        }
+        return gameover;
+       
+    }
+    
+    
 }
